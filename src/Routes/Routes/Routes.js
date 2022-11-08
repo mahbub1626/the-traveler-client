38fc6caa1cf49f8c0bca3089ec9269810e14ff6d
mahbub1626 <mahbub1626@gmail.com>
2022-11-08 16:36:18 +0600

    navbar added

---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,42 @@
+import { createBrowserRouter } from "react-router-dom";
+import Error from "../../components/Error/Error";
+import Home from "../../components/Home/Home";
+import Main from "../../layout/Main";
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+    //   {
+    //     path: '/login',
+    //     element: <LogIn></LogIn>
+    //   },
+    //   {
+    //     path: '/signup',
+    //     element: <SignUp></SignUp>
+    //   },
+    //   {
+    //     path: '/checkout/:id',
+    //     element: <PrivateRoutes><Checkout></Checkout></PrivateRoutes>,
+    //     loader: ({ params }) => fetch(`http://localhost:5000/services/${params.id}`)
+    //   },
+    //   {
+    //     path: '/orders',
+    //     element: <PrivateRoutes><Orders></Orders></PrivateRoutes>
+    //   },
+      {
+        path: '/*',
+        element: <Error></Error>
+      },
+    ]
+  }
+])
+
+export default router;
