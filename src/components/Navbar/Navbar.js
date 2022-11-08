26ff657d723f6d6da4ab6546e719ad0526df95b8
mahbub1626 <mahbub1626@gmail.com>
2022-11-08 20:17:21 +0600

    user logOut added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,14 +3,17 @@ import { Link } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const Navbar = () => {
-    const { user } = useContext(AuthContext);
+    const { user, logOut } = useContext(AuthContext);
 
     const menuItems = <>
         <li><Link to='/'>Home</Link></li>
         
         {
             user?.uid ?
+                <>
                 <li><Link to='/orders'>Order</Link></li>
+                <li><Link onClick={logOut}>Log out</Link></li>
+                </>
                 :
                 <li><Link to='/login'>Login</Link></li>
         }
