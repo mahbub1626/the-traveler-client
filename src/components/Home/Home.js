b1ee939effe1b6c6c9a50fb5c3b424c8b4413fa6
mahbub1626 <mahbub1626@gmail.com>
2022-11-09 14:40:32 +0600

    card splice

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Services from '../Services/Services/Services';
+import ServicesCard from '../Services/Services/ServicesCard';
 import Banner from './Banner/Banner';
 
 const Home = () => {
+    const [services, setServices] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:5000/services')
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data);
+                // setServices(data);
+                const result = data.splice(0, 3)
+
+                setServices(result);
+                console.log(result);
+
+            })
+    }, [])
     return (
         <div>
             <Banner></Banner>
             <h2>This is home page</h2>
-            <Services></Services>
+            <div className='my-8 '>
+                <h1 className='text-3xl my-4'>Services: {services.length} </h1>
+
+                <div className='grid lg:grid-cols-3 gap-4 lg:gap-8'>
+                    {
+                        services.map(service => <ServicesCard
+                            key={service._id}
+                            service={service}
+                        ></ServicesCard>)
+                    }
+                </div>
+                <div className="w-36  my-4 mx-auto">
+                    <Link to='/services'>
+                        <button className="btn btn-primary  mx-auto">See more</button>
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Services/Services/Services.js
+++ src/components/Services/Services/Services.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import ServicesCard from './ServicesCard';
 
 const Services = () => {
@@ -10,12 +11,18 @@ const Services = () => {
             .then(data => {
                 // console.log(data);
                 setServices(data);
+                // const result = data.splice(0, 3)
+
+                // setServices(result);
+                // console.log(result);
+
             })
     }, [])
     return (
-        <div className='my-8'>
-            <h1>Services: {services.length} </h1>
-            <div className='grid lg:grid-cols-3'>
+        <div className='my-8 '>
+            <h1 className='text-3xl my-4'>Services: {services.length} </h1>
+
+            <div className='grid lg:grid-cols-3 gap-4 lg:gap-8'>
                 {
                     services.map(service => <ServicesCard
                         key={service._id}
@@ -24,8 +31,9 @@ const Services = () => {
                 }
             </div>
             <div className="w-36  my-4 mx-auto">
-                <button className="btn btn-primary  mx-auto">See more</button>
-
+                {/* <Link to='/services'>
+                    <button className="btn btn-primary  mx-auto">See more</button>
+                </Link> */}
             </div>
         </div>
     );
